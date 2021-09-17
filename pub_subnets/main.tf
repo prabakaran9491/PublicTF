febab9dbda45d2b6f4c1f5a5bcc01fee06c8b416
prabakaran9491 <prabakar53@gmail.com>
2021-09-17 14:19:07 +0530

    added

--- pub_subnets/main.tf
+++ pub_subnets/main.tf
@@ -4,6 +4,7 @@ resource "aws_subnet" "Public_subnet" {
   cidr_block = var.cidr_block_publicsubnet(count.index)
   map_public_ip_on_launch =var.map_public_ip_on_launch
   tags = {
-    Name = "Name" = format("${var.public_subnet}-%d", count.index + 1)
+    Name = format("${var.public_subnet}-%d", count.index + 1)
   }
 }
+
