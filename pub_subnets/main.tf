e57822b82c0621c5ef6df788b746c2ae1114853d
prabakaran9491 <prabakar53@gmail.com>
2021-09-21 21:09:22 +0530

    added

--- pub_subnets/main.tf
+++ pub_subnets/main.tf
@@ -1,8 +1,8 @@
-resource "aws_subnet""Public_subnet" {
+resource "aws_subnet" "Public_subnet" {
   count      = var.pub_subnet_count
   vpc_id     = var.vpc_id
   cidr_block = var.cidr_block_publicsubnet[count.index]
-  map_public_ip_on_launch =var.map_public_ip_on_launch
+  map_public_ip_on_launch = var.map_public_ip_on_launch
   tags = {
     Name = format("${var.public_subnet}-%d", count.index + 1)
   }
